6d4a433d14518ac94f32f873803e74dce9d5169e
Midul Mahmud <midul.mahmud20@gmail.com>
2026-09-12 19:00:48 +0600

    add Nav Bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,9 +2,9 @@ import Logo from '../assets/logo-text.png'
 
 const Nav = () => {
     return (
-        <nav className='bg-amber-800'>
-        <div className='container mx-auto flex justify-between items-center py-3'>
-            <img src={Logo} alt="DevStack" />
+        <nav className=''>
+        <div className='container mx-auto flex justify-between items-center py-4'>
+            <img src={Logo} alt="DevStack" className='w-[130px]' />
             <ul className='flex gap-5'>
                 <li><a href="#"></a>Home</li>
                 <li><a href="#"></a>Technologies</li>
@@ -12,9 +12,9 @@ const Nav = () => {
                 <li><a href="#"></a>About</li>
                 <li><a href="#"></a>Contact</li>
             </ul>
-            <div>
-            <button className="btn btn-soft btn-secondary">Secondary</button>
-            <button className="btn btn-soft btn-secondary">Secondary</button>
+            <div className='flex gap-3'>
+            <button className=" ">Sign In</button>
+            <button className="btn btn-active btn-secondary rounded-2xl">Sign Up</button>
             </div>
         </div>
         </nav>
