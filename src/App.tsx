40010bab7a825ed7d038bcdae80c720bb67bcb09
Midul Mahmud <midul.mahmud20@gmail.com>
2026-09-13 10:03:09 +0600

    fixed path of public folder

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Nav from "./components/Nav";
 import type { IStacks } from "./Types/types";
 
 const stacksPromise = async () : Promise<IStacks[]>  => {
-  const res = await fetch ("/public/data.json")
+  const res = await fetch ("/data.json")
   const data = await res.json()
   return data;
 }
